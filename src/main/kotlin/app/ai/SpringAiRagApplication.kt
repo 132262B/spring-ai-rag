429e2d5099a3daac82fd717769fd47b8b6a674f3
Igor <132262b@naver.com>
2024-07-20 11:29:08 +0900

    init project

---
+++ src/main/kotlin/app/ai/SpringAiRagApplication.kt
@@ -0,0 +1,11 @@
+package app.ai
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringAiRagApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringAiRagApplication>(*args)
+}
---
+++ src/test/kotlin/app/ai/SpringAiRagApplicationTests.kt
@@ -0,0 +1,13 @@
+package app.ai
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringAiRagApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
